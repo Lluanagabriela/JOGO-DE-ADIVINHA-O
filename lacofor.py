2ab9d9300dca900c973eaafd3e245286b17e7b61
Luana <128156531+Lluanagabriela@users.noreply.github.com>
2024-03-06 09:50:48 -0300

    Add files via upload

---
+++ lacofor.py
@@ -0,0 +1,17 @@
+nomes = ['João','Luiz','Levi']
+for nome in nomes:
+    print(nome)
+
+print('*********************')
+
+soma = 0
+for i in range (1,11):
+    soma = soma + 1
+    print(soma)
+print ('*********************')
+
+n = 5
+fatorial = 1
+for i in range (1, n+1):
+    fatorial = fatorial * 1
+print(f'O fatorial de {n} é {fatorial}')
