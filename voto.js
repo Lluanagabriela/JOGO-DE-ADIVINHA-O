ea66efa85275622fc36921d091fe8f52610e031b
Luana <128156531+Lluanagabriela@users.noreply.github.com>
2024-03-11 09:02:11 -0300

    Add files via upload

---
+++ voto.js
@@ -0,0 +1,11 @@
+// variaveis representam idade e cidadania 
+let idade= 18;
+let cidadaniaBrasileiro = true;
+
+// verificar se a idade é maior ou igual a 18 e se a cidadania é brasileira (true)
+
+if (idade >= 16 && cidadaniaBrasileiro == true) {
+    console.log ('pode votar');
+}else{
+    console.log ('não pode votar');
+}
