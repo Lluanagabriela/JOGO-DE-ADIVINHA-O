ccb44fb2d867d2808b4756eb1134ba3b022faddb
Luana <128156531+Lluanagabriela@users.noreply.github.com>
2024-05-15 09:03:19 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -1,40 +1,54 @@
 import random;
 
-print('*********************************')
-print('Bem vindo, ao JOGO DE ADIVINHAÇÃO')
-print('*********************************')
+print('**********************************')
+print('Bem vindo, ao JOGO DE ADIVINHAÇÃ0')
+print('**********************************')
 
 #Definindo o número secreto
 numeroSecreto = random.randrange(1,101)
 #print(numeroSecreto)
-#Definindo o número de tentativas e rodada
-numeroTentativas = 10
+#Definindo o número de tentativas
+numeroTentativas = 0
 rodada = 1
- 
-print("Qual nível de dificuldade vocẽ deseja jogar?")
-print("(1) - Fácil, (2) - Médio, (3) - Difícil")
+pontos = 1000
 
-nivel = int(input("Defina o nível: "))
-while(rodada <= numeroTentativas):
+print('Qual o nível de dificuldade?')
+print('(1)-Fácil, (2)-Médio, (3)-Difícil, (4)-Hacker')
+
+nível = int(input('Defina o nível: '))
+
+
+#Vamos mudar o número de tentativas conforme a dificuldade
+if(nível == 1):
+    numeroTentativas = 15
+elif(nível == 2):
+    numeroTentativas = 10
+elif(nível == 3):
+    numeroTentativas = 5   
+else:
+    numeroTentativas = 3     
+
+while(rodada<= numeroTentativas):
     print('Tentativa',rodada, 'de' , numeroTentativas)
 
 #Recebendo o chute do jogador
     chuteString = input('Digite um número entre 1 e 100: ')
     chute = int(chuteString)
 
-#Declarando as condições
+#Declarando as condições 
     if (numeroSecreto == chute):
-        print('Você acertou!')
-        break
+        print('Você acertou!!!!')
+        Break
     elif(chute>numeroSecreto):
         print('Você errou!! O número secreto é um número menor')
     else:
-        print('Você errou!! O número secreto é um número maior')
+        print('Você errou!!! O número secreto é um número maior')
+
+pontos_perdidos = abs (numeroSecreto - chute);      
+pontos = pontos - pontos_perdidos  
 
     #numeroTentativas = numeroTentativas - 1
-    rodada = rodada + 1
+    rodada = rodada + 1 
+    #Aula Elif 26.02.24  
 
-
-
-#Aula Elif 26.02.24
 
